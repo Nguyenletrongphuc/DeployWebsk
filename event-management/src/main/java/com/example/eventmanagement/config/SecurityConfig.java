1c6b463334a67e9d33198028135ee08cf61ffeae
Nguyenletrongphuc <ecchiphuc@gmail.com>
2026-04-02 15:12:06 +0700

    them duong dan cloud2

--- event-management/src/main/java/com/example/eventmanagement/config/SecurityConfig.java
+++ event-management/src/main/java/com/example/eventmanagement/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "http://localhost:3000", "https://deploywebsk-2.onrender.com/"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "http://localhost:3000", "https://deploy-websk.vercel.app/","https://deploywebsk-1.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
